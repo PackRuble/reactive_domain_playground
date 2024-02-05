958ea5d1253375fae2de54bfd3a7f7961a89c267
Ruble <pack.ruble@gmail.com>
2024-02-05 13:37:36 +0300

    add: hash in provider logs

--- lib/src/domain/riverpod_logger.dart
+++ lib/src/domain/riverpod_logger.dart
@@ -2,19 +2,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'log_notifier.dart' show xlog;
 
-class RiverpodObserver extends ProviderObserver{
+class RiverpodObserver extends ProviderObserver {
   const RiverpodObserver();
 
-  Function(String) get onLog  => xlog;
+  Function(String) get onLog => xlog;
 
   @override
   void didAddProvider(
-      ProviderBase provider,
-      Object? value,
-      ProviderContainer container,
-      ) {
+    ProviderBase provider,
+    Object? value,
+    ProviderContainer container,
+  ) {
     if (provider.name != null) {
-      onLog('[D] INIT: ${provider.name}'
+      onLog('[D] INIT: ${provider.name} ${_withHash(provider)}'
           '\n╔══'
           '\n║ value: $value'
           '\n╚══');
@@ -23,13 +23,13 @@ class RiverpodObserver extends ProviderObserver{
 
   @override
   void didUpdateProvider(
-      ProviderBase provider,
-      Object? previousValue,
-      Object? newValue,
-      ProviderContainer container,
-      ) {
+    ProviderBase provider,
+    Object? previousValue,
+    Object? newValue,
+    ProviderContainer container,
+  ) {
     if (provider.name != null) {
-      onLog('[D] UPD: ${provider.name}'
+      onLog('[D] UPD: ${provider.name} ${_withHash(provider)}'
           '\n╔══'
           '\n║ previousValue: $previousValue'
           '\n║ newValue: $newValue'
@@ -40,21 +40,25 @@ class RiverpodObserver extends ProviderObserver{
   /// A provider was disposed.
   @override
   void didDisposeProvider(
-      ProviderBase provider,
-      ProviderContainer container,
-      ) {
+    ProviderBase provider,
+    ProviderContainer container,
+  ) {
     if (provider.name != null) {
-      onLog('[D] Dispose: ${provider.name}');
+      onLog('[D] DISPOSE: ${provider.name} ${_withHash(provider)}');
     }
   }
 
   @override
   void providerDidFail(
-      ProviderBase provider,
-      Object error,
-      StackTrace stackTrace,
-      ProviderContainer container,
-      ) {
-    onLog('[D] FAIL: ${provider.name}, $error, $stackTrace');
+    ProviderBase provider,
+    Object error,
+    StackTrace stackTrace,
+    ProviderContainer container,
+  ) {
+    onLog(
+      '[D] FAIL: ${provider.name} ${_withHash(provider)}, $error, $stackTrace',
+    );
   }
+
+  String _withHash(ProviderBase provider) => '{hash=${provider.hashCode}}';
 }
